31defffe31c2f69f339fe6d49a075ff3ab3812af
chino0rt24 <ortegapablo0224@gmial.com>
2023-08-11 00:45:49 -0600

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes realizar la lógica de inicio de sesión con el correo electrónico y la contraseña ingresados.
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  };
+
+  const formStyle = {
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+    width: '300px',
+  };
+
+  const inputStyle = {
+    width: '100%',
+    padding: '10px',
+    marginBottom: '10px',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+  };
+
+  const buttonStyle = {
+    width: '100%',
+    padding: '10px',
+    backgroundColor: '#007BFF',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  };
+
   return (
-    <div>
-      <h1>Welcome to Your Calendar</h1>
-      <button>Create New Appointment</button>
-      <div>
-        {/* Aquí colocaremos el componente de Calendario */}
-      </div>
-      <div>
-        {/* Aquí colocaremos el componente de Lista de Citas */}
-      </div>
+    <div style={containerStyle}>
+      <form style={formStyle} onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <input
+          style={inputStyle}
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+        <input
+          style={inputStyle}
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+        <button style={buttonStyle} type="submit">Login</button>
+      </form>
     </div>
   );
-}
+};
 
 export default Login;
